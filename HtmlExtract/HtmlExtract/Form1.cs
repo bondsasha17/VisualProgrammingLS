77afbdbac49e4ec26d69c7d7232a5e922ee3d08b
bondsasha17 <bondarenko.sasha2013@yandex.ua>
2016-03-13 18:54:19 +0200

    ExtractMethod
    implemented method for extracting text from tags

--- HtmlExtract/HtmlExtract/Form1.cs
+++ HtmlExtract/HtmlExtract/Form1.cs
@@ -20,28 +20,17 @@ namespace HtmlExtract
 
         private void btnPick_Click(object sender, EventArgs e)
         {
-            string FilePath;
+            HtmlFile MyFile;
             OpenFileDialog ofd = new OpenFileDialog();
-           if  (ofd.ShowDialog()== DialogResult.OK)
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                MyFile = new HtmlFile(ofd.FileName);
+                string headText = MyFile.Extract(HtmlFile.Tag.head);
+                string bodyText = MyFile.Extract(HtmlFile.Tag.body);
+
+                MessageBox.Show(headText + "\n" + bodyText);
+            }
             
-               FilePath = ofd.FileName;
-           StreamReader sr = new StreamReader(FilePath = ofd.FileName);
-               string FileText = sr.ReadToEnd();
-               StringBuilder sb = new StringBuilder(FileText);
-               int startHead = FileText.IndexOf("<head>") + 6;
-               int endHead = FileText.IndexOf("</head>");
-               string head = FileText.Substring(startHead, endHead - startHead);
-               int startBody = FileText.IndexOf("<body>") + 6;
-               int endBody = FileText.IndexOf("</body>");
-               string body = FileText.Substring(startBody, endBody - startBody);
-
-
-               MessageBox.Show(head + body);
-           }
-
-       
-      }
-
-      
+        }
     }
-
+}
---
+++ HtmlExtract/HtmlExtract/HtmlFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace HtmlExtract
+{
+    public class HtmlFile
+    {
+        public string filePath;
+
+        public enum Tag { head, title, body, a, p, html};
+        
+        public HtmlFile ( string filePath)
+        {
+            this.filePath = filePath;
+        }
+        
+        public string Extract(Tag tag)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string FileText = sr.ReadToEnd();
+
+            string tagName = Enum.GetName(typeof(Tag), tag);
+            int startTag = FileText.IndexOf("<" + tagName + ">") + tagName.Length + 2;
+            int endTag = FileText.IndexOf("</" + tagName + ">");
+            string tagText = FileText.Substring(startTag, endTag - startTag);
+
+            while (tagText.IndexOf("<") != -1)
+            {
+                int startdeletTag = tagText.IndexOf("<");
+                int enddeletTag = tagText.IndexOf(">");
+                tagText = tagText.Remove(startdeletTag, enddeletTag - startdeletTag + 1);
+            }
+
+            return tagText;
+        }
+
+
+    }
+}
